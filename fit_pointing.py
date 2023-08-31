6f11ddac3239e72fec5c1855308a93094fd5e321
Yoshinori Sueno <yoshinori0778@gmail.com>
2023-08-31 14:29:02 +0900

    add initial pointing_model for full array

---
+++ fit_pointing.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import sys
+import datetime
+sys.path.append('/data/ysueno/home/workspace/script/')
+from gb_cal.misc.misc import read_pkl, save_pkl, _moon_pos
+from lmfit import minimize, Parameters, fit_report
+from Pointing_cal.pointing_new import delA, delE
+# pointing fit for full array
+
+
+def calc_moon_altaz(ts):
+    alt = []
+    az = []
+    for it in ts:
+        moon_altaz = _moon_pos(datetime.datetime.fromtimestamp(it, tz = datetime.timezone.utc))
+        alt.append(moon_altaz.alt.deg)
+        az.append(moon_altaz.az.deg)        
+    return alt, az
+
+def set_params(chips, kidids):
+    chip_kidids = []
+    for ichip, ikidid in zip(chips, kidids):
+        ichip_kidid = ichip + f'kid{ikidid:02}'
+        if ichip_kidid in chip_kidids:
+            pass
+        else:
+            chip_kidids.append(ichip_kidid)
+    params = Parameters()
+    for iy, ichip_kidid in enumerate(chip_kidids):
+        params.add('IA' + ichip_kidid, value = -55, min = -90, max = -20)
+        params.add('IE' + ichip_kidid, value = 0, min = -11, max = 11)
+        #params.add('IA' + ichip_kidid, value = 50.5 - 360)
+        #params.add('IE' + ichip_kidid, value = 0)
+    
+    params.add('AN', value = 0 , min = -1, max = 1)
+    params.add('AW', value = 0, min = -1, max = 1)
+    return params
+
+def set_params_fix(chips, kidids, AN = -0.03067157, AW = -0.04660315):
+    chip_kidids = []
+    for ichip, ikidid in zip(chips, kidids):
+        ichip_kidid = ichip + f'kid{ikidid:02}'
+        if ichip_kidid in chip_kidids:
+            pass
+        else:
+            chip_kidids.append(ichip_kidid)
+    params = Parameters()
+    for iy, ichip_kidid in enumerate(chip_kidids):
+        params.add('IA' + ichip_kidid, value = -55, min = -90, max = -20)
+        params.add('IE' + ichip_kidid, value = 0, min = -11, max = 11)
+        #params.add('IA' + ichip_kidid, value = 50.5 - 360)
+        #params.add('IE' + ichip_kidid, value = 0)
+    
+    params.add('AN', value = AN, vary = False)
+    params.add('AW', value = AW, vary = False)
+    return params
+
+def residual_single(params, chip, kidid, el_diff, az_diff, el_moon, az_moon, theta_errmax = 1, phi_errmax = 1):
+    chip_kidid = chip + f'kid{kidid:02}'
+    IA = params['IA' + chip_kidid]
+    IE = params['IE' + chip_kidid]
+    AN = params['AN']
+    AW = params['AW'] 
+    az_model = delA(el_moon, az_moon, IA, AN, AW)
+    el_model = delE(az_moon, IE, AN, AW)
+    
+    err = np.sqrt(phi_errmax**2 + theta_errmax**2)
+    #err = 0.053    
+    #err = 1
+    el_resi = (el_diff - el_model)/err
+    az_resi = np.cos(np.deg2rad(el_moon))*(az_diff - az_model)/err
+    
+    #return np.sqrt(el_resi**2 + az_resi**2)
+    return el_resi, az_resi
+
+def residual(params, chips, kidids, el_diffs, az_diffs, el_moons, az_moons):
+    resis = [residual_single(params, ichip, ikidid, iel_diff, iaz_diff, iel_moon, iaz_moon)
+             for ichip, ikidid, iel_diff, iaz_diff, iel_moon, iaz_moon
+             in zip(chips, kidids, el_diffs, az_diffs, el_moons, az_moons)]
+    #print(resis)
+    #print(len(resis))
+    #return np.array(resis)
+    return np.array(np.concatenate(resis))
+
+def fit_exe(dffit):
+    params = set_params(dffit['chip'], dffit['kidid'])
+    out = minimize(residual, 
+                   params, 
+                   args = (dffit['chip'],dffit['kidid'],dffit['el_diff'],dffit['az_diff'],dffit['el_moon'],dffit['az_moon']), 
+                   scale_covar=False)
+    return out
+    
+    
+def fit_exe_fix(dffit, AN = -0.03067157, AW = -0.04660315):
+    params = set_params_fix(dffit['chip'], dffit['kidid'], AN, AW)
+    out = minimize(residual, 
+                   params, 
+                   args = (dffit['chip'],dffit['kidid'],dffit['el_diff'],dffit['az_diff'],dffit['el_moon'],dffit['az_moon']), 
+                   scale_covar=False)
+    return out
--- pointing_new.py
+++ pointing_new.py
@@ -15,34 +15,35 @@ def read_pkl(path):
 #RESULTP = '/data/ysueno/home/workspace/script/gb_cal/pointing/pointing_params_202205.pkl'
 #RESULTP = '/data/ysueno/home/workspace/script/Pointing_cal/pointing_params_20230703.pkl'
 #RESULTP = '/data/ysueno/home/workspace/script/Pointing_cal/pointing_params_20230706.pkl'
-RESULTP = '/data/ysueno/home/workspace/script/Pointing_cal/pointing_params_20230722.pkl'
+#RESULTP = '/data/ysueno/home/workspace/script/Pointing_cal/pointing_params_20230722.pkl'
+RESULTP = '/data/ysueno/home/workspace/script/Pointing_cal/pointing_params_20230826.pkl'
 
 #KIDIDS = np.array([ 0,  1,  2,  3,  4,  5,  6,  7, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25])
-KIDIDS = np.array([ 0,  2,  3,  4,  5,  6,  7, 8, 10, 11, 12, 14, 16, 19, 20, 21, 22, 23, 25]) # from 20230706
+#KIDIDS = np.array([ 0,  2,  3,  4,  5,  6,  7, 8, 10, 11, 12, 14, 16, 19, 20, 21, 22, 23, 25]) # from 20230706
 
-def delA(el, az, IA, AN, AW, ifprint = False):
+def delA(el, az, IA, AN, AW):
     return IA + AN*np.tan(np.deg2rad(el))*np.sin(np.deg2rad(az)) - AW*np.tan(np.deg2rad(el))*np.cos(np.deg2rad(az))
 
-def delA_til(el, az, AN, AW, ifprint = False):
+def delA_til(el, az, AN, AW):
     return AN*np.tan(np.deg2rad(el))*np.sin(np.deg2rad(az)) - AW*np.tan(np.deg2rad(el))*np.cos(np.deg2rad(az))
 
-def delE(az, IE, AN, AW, ifprint = False):
+def delE(az, IE, AN, AW):
     return IE + AN*np.cos(np.deg2rad(az)) + AW*np.sin(np.deg2rad(az))
 
-def delE_til(az, AN, AW, ifprint = False):
+def delE_til(az, AN, AW):
     return AN*np.cos(np.deg2rad(az)) + AW*np.sin(np.deg2rad(az))
 
-def cal_encoder2model(az, el, kidid, fitr):
-    az1 = az - delA(el, az, fitr['IA'+f'{kidid:02}'], fitr['AN'], fitr['AW'])
-    el1 = el - delE(az, fitr['IE'+f'{kidid:02}'], fitr['AN'], fitr['AW'])
+def cal_encoder2model(az, el, chip_kidid, fitr):
+    az1 = az - delA(el, az, fitr['IA' + chip_kidid], fitr['AN'], fitr['AW'])
+    el1 = el - delE(az, fitr['IE' + chip_kidid], fitr['AN'], fitr['AW'])
     return az1%360, el1%360
 
-def cal_encoder2model_rep_dif(az, el, kidid, fitr):
+def cal_encoder2model_rep_dif(az, el, fitr):
     azdif = delA_til(el, az, fitr['AN'], fitr['AW'])
     eldif = delE_til(az, fitr['AN'], fitr['AW'])
     return azdif,eldif
 
-def pointing_cal(az, el, kidid, num = 2, encoder = True, resultp = RESULTP):
+def pointing_cal_new(az, el, chip, kidid, num = 3, encoder = True, resultp = RESULTP):
     '''Calculate Az/El after pointing calibration.
     Parameters
     ----------
@@ -50,6 +51,8 @@ def pointing_cal(az, el, kidid, num = 2, encoder = True, resultp = RESULTP):
         Azimuth value [deg]
     el: array-like
         Elevation value [deg]
+    chip : str
+        Chip ID (1A, 1B, 2A, ..., 220)
     kidid: int
         KIDID for calculation.
     num: int
@@ -64,22 +67,23 @@ def pointing_cal(az, el, kidid, num = 2, encoder = True, resultp = RESULTP):
     elevation: array-like
         Elevation array after pointing calibration
     '''
-
-    assert kidid in KIDIDS, 'There is no poinitng cal data. Please make sure the kidid.'
     fitr = read_pkl(resultp)
+    chip_kidid = chip + f'kid{kidid:02}'
+    assert 'IA' + chip_kidid in fitr.keys(), 'There is no poinitng cal data. Please make sure the chipID and kidid.'
+    
     # Azimuth direction of GroundBIRD encoder is opposite direction to AltAz coordinate.
     if encoder:
         az = (-az)%360
 
-    az1 = (az-fitr['IA' + f'{kidid:02}'])%360
-    el1 = (el-fitr['IE' + f'{kidid:02}'])%360
+    az1 = (az-fitr['IA' + chip_kidid])%360
+    el1 = (el-fitr['IE' + chip_kidid])%360
     for i in range(num):
         if i ==0:
-            azdif, eldif = cal_encoder2model_rep_dif(az1, el1, kidid, fitr)
+            azdif, eldif = cal_encoder2model_rep_dif(az1, el1, fitr)
             az2 = az1 - azdif
             el2 = el1 - eldif
         else:
-            azdif, eldif = cal_encoder2model_rep_dif(az2, el2, kidid, fitr)
+            azdif, eldif = cal_encoder2model_rep_dif(az2, el2, fitr)
             az2 = az1 - azdif
             el2 = el1 - eldif
     return (az2)%360, (el2)%360
@@ -88,15 +92,17 @@ def pointing_cal(az, el, kidid, num = 2, encoder = True, resultp = RESULTP):
 
 ###### script for validation ########
 
-def cal_model2encoder(az, el, kidid, fitr):
-    az1 = az + delA(el, az, fitr['IA'+f'{kidid:02}'], fitr['AN'], fitr['AW'])
-    el1 = el + delE(az, fitr['IE'+f'{kidid:02}'], fitr['AN'], fitr['AW'])
+def cal_model2encoder(az, el, chip, kidid, fitr):
+    chip_kidid = chip + f'kid{kidid:02}'
+    az1 = az + delA(el, az, fitr['IA' + chip_kidid], fitr['AN'], fitr['AW'])
+    el1 = el + delE(az, fitr['IE' + chip_kidid], fitr['AN'], fitr['AW'])
     return az1%360, el1%360
 
-def compare_input_manytime(az, el, kidid, fitr, num):
-    az1, el1 = cal_model2encoder(az, el, kidid, fitr)
-    az2 = (az1-fitr['IA' + f'{kidid:02}'])%360
-    el2 = (el1-fitr['IE' + f'{kidid:02}'])%360
+def compare_input_manytime(az, el, chip, kidid, fitr, num):
+    chip_kidid = chip + f'kid{kidid:02}'
+    az1, el1 = cal_model2encoder(az, el, chip, kidid, fitr)
+    az2 = (az1-fitr['IA' + chip_kidid])%360
+    el2 = (el1-fitr['IE' + chip_kidid])%360
     for i in range(num):
         if i ==0:
             azdif, eldif = cal_encoder2model_rep_dif(az2, el2, kidid, fitr)
